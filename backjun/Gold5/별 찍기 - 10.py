b13288728f6dced4e033d041ddee1aeb482b74a6
JunYupK <top1506@naver.com>
2023-02-07 16:33:06 +0900

    2023-02-07

--- backjun/Gold5/별 찍기 - 10.py
+++ backjun/Gold5/별 찍기 - 10.py
@@ -1,11 +1,9 @@
 def draw_star(n):
     global Map
-
     if n == 3:
         Map[0][:3] = Map[2][:3] = [1] * 3
         Map[1][:3] = [1, 0, 1]
         return
-
     a = n // 3
     draw_star(n // 3)
     for i in range(3):
@@ -14,10 +12,7 @@ def draw_star(n):
                 continue
             for k in range(a):
                 Map[a * i + k][a * j:a * (j + 1)] = Map[k][:a]  # 핵심 아이디어
-
-
 N = int(input())
-
 # 메인 데이터 선언
 Map = [[0 for i in range(N)] for i in range(N)]
 
