8665853b6d64d7703b8a682b3c68cbd3976431ea
김준엽 <53340295+JunYupK@users.noreply.github.com>
2023-04-26 18:32:38 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if(a == b && b == c){
+            answer += (a + b + c) * (Math.pow(a,2) + Math.pow(b,2) + Math.pow(c,2)) * (Math.pow(a,3) + Math.pow(b,3) + Math.pow(c,3));
+        }
+        else if(a != b && b != c && a != c){
+            answer += a+b+c;
+        }
+        else{
+            answer += (a+b+c) * (Math.pow(a,2) + Math.pow(b,2) + Math.pow(c,2));
+        }
+        return answer;
+    }
+}
