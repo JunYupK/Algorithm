dd3249409993c7ba37be180d55a3af18ebc4dca5
김준엽 <53340295+JunYupK@users.noreply.github.com>
2023-04-26 17:18:19 +0900

    [unrated] Title: 공배수, Time: 0.01 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if(number % n == 0 && number % m == 0){
+            return 1;
+        }
+        return answer;
+    }
+}
